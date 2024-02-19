c778ac7a8f4d573ab6e4069b2217366f1c895963
JM-arlopes <jmiguelarlopes@gmail.com>
2024-02-18 22:27:43 -0300

    inserindo o BannerHome

--- application-react/src/components/pages/Home/index.jsx
+++ application-react/src/components/pages/Home/index.jsx
@@ -1,6 +1,12 @@
+import AboutHome from "../../home/AboutHome";
 import BannerHome from "../../home/BannerHome";
 import styles from "./home.module.css";
 
 export default function Home() {
-  return <BannerHome />;
+  return (
+    <>
+      <BannerHome />
+      <AboutHome />
+    </>
+  );
 }
